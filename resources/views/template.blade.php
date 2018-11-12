0718e21410edfea22a771ea551243a701c389d46
lehon aritonang <lehon_25@yahoo.com>
2018-11-12 23:50:20 +0700

    create template and navigation blade

---
+++ resources/views/template.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link href="{{ asset('css/app.css') }}" rel= "stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <a class="navbar-brand" href="">Laravel Answer</a>
+
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                          <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
+                          <li class="nav-item active">
+                              <a class="nav-link" href="{{ route('index') }}">Home </a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Recent</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link disabled" href="#">Populer</a>
+                            </li>
+                          </ul>
+                          <ul class="nav navbar-nav navbar-right">
+                                  <a href="" class="btn btn-primary" style="margin-top:5px;">Ask A Question</a>
+
+                                </ul>
+                              </div><!-- /.navbar-collapse -->
+                            </div><!-- /.container-fluid -->
+                          </nav>
+                          @yield('content')
+
+    </body>
+</html>
