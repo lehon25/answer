601b938029b779e2eb3e056798af86865eb4378c
lehon aritonang <lehon_25@yahoo.com>
2018-11-12 23:50:43 +0700

    create template and navigation blade

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,32 +8,7 @@
         <link href="{{ asset('css/app.css') }}" rel= "stylesheet" type="text/css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <a class="navbar-brand" href="">Laravel Answer</a>
-
-                        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                          <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
-                          <li class="nav-item active">
-                              <a class="nav-link" href="{{ route('index') }}">Home </a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="#">Recent</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link disabled" href="#">Populer</a>
-                            </li>
-                          </ul>
-                          <ul class="nav navbar-nav navbar-right">
-                                  <a href="" class="btn btn-primary" style="margin-top:5px;">Ask A Question</a>
-
-                                </ul>
-                              </div><!-- /.navbar-collapse -->
-                            </div><!-- /.container-fluid -->
-                          </nav>
+                    @include('_includes/nav/topnav')
                           @yield('content')
 
     </body>
